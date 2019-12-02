193a67f22cbbc3e1f557d6a1d71d544db42cd1f0
mina <mp922352612@gmail.com>
2019-12-03 02:06:47 +0800

    php with bootstrap

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'includes/head.php' ; ?>
+</head>
+<body>
+  <?php $page = 'home' ;include 'includes/navbar.php' ; ?>
+
+	<!--- Image Slider -->
+	<div class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active"><img src="img/laptop.png"></div>
+			<div class="carousel-item"><img src="img/sky.png"></div>
+			<div class="carousel-item"><img src="img/bridge.png"></div>
+		</div>
+	</div>
+	<!--- End Image Slider -->
+
+	<!--- BOOTSTRAP UDEMY COURSE -->
+	<div class="container">
+		<div class="row justify-content-center text-center">
+			<div class="col-10 py-5">
+				<h2>Advanced Bootstrap Theme Udemy Course</h2>
+				<p class="lead">Bootstrap is an open-source front-end library with HTML and CSS based designs. This website is built with HTML5, CSS3 and Bootstrap 4.</p><a class="btn btn-purple btn-lg" href="https://w3newbie.com/courses/" target="_blank">W3NEWBIE COURSES</a>
+			</div>
+		</div>
+	</div>
+	<!--- BOOTSTRAP UDEMY COURSE -->
+
+	<!--- Start Jumbotron -->
+	<div class="jumbotron">
+		<div class="container">
+			<h2 class="text-center pt-5 pb-3">BUILT WITH THE BEST</h2>
+			<div class="row justify-content-center text-center">
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/html5.png">
+						<h3>HTML5</h3>
+						<p>HTML5 is the fifth and current major version of the HTML standard, and subsumes XHTML.</p>
+					</div>
+				</div>
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/bootstrap4.png">
+						<h3>BOOTSTRAP 4</h3>
+						<p>Bootstrap is an open-source front-end library with HTML and CSS based designs.</p>
+					</div>
+				</div>
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/css3.png">
+						<h3>CSS3</h3>
+						<p>CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.</p>
+					</div>
+				</div>
+			</div><!--- End Row -->
+		</div><!--- End Container -->
+	</div>
+	<!--- End Jumbotron -->
+
+	<!--- Two Column Section -->
+	<div class="container py-3">
+		<div class="row justify-content-center py-5">
+			<div class="col-lg-6">
+				<h3 class="pb-4">Learn to build Bootstrap websites</h3>
+				<p>The columns will automatically stack on top of each other when the screen is less than 992px wide.</p>
+				<p>Resize the browser window to see the effect. Responsive web design has become more important as the amount of mobile traffic now accounts for more than half of total internet traffic.</p><a class="btn btn-purple btn-lg" href="https://w3newbie.com/courses/">W3NEWBIE COURSES</a>
+			</div>
+			<div class="col-lg-6"><img class="img-fluid" src="img/responsive.png"></div>
+		</div>
+	</div>
+	<!--- End Two Column Section -->
+
+	<?php include 'includes/footer.php' ; ?>
+
+  <?php include 'includes/scripts.php' ; ?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+ <!--- Check out w3newbie Templates & Courses! -->
+ <div style="float:left; max-width: 48%; margin: .5rem; padding: 0; border: .5rem solid #666;">
+ 	<a href="https://w3newbie.com/templates/" target="_blank" style="z-index: 100; cursor: pointer!important;"><img src="https://www.w3newbie.com/wp-content/uploads/w3newbie-templates-zip.png" style="max-width: 100%; min-width: 100%;"></a>
+</div>
+ <div style="float:left; max-width: 48%; margin: .5rem; padding: 0; border: .5rem solid #666;">
+ 	<a href="https://w3newbie.com/courses/" target="_blank" style="z-index: 100; cursor: pointer!important;"><img src="https://www.w3newbie.com/wp-content/uploads/w3newbie-courses-zip.png" style="max-width: 100%; min-width: 100%;"></a>
+</div>
+<br style="clear: both;">
+<div style="max-width: 52%; margin: 2rem auto; padding: 0; border: .5rem solid #666;">
+ 	<a href="https://w3newbie.com/hosting/" target="_blank" style="z-index: 100; cursor: pointer!important;"><img src="https://www.w3newbie.com/wp-content/uploads/web-hosting-zip.jpg" style="max-width: 100%; min-width: 100%;"></a>
+</div>
+ <!--- Check out w3newbie Templates & Courses! -->
---
+++ portfolio.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'includes/head.php' ; ?>
+</head>
+<body> 
+  <?php $page = 'portfolio'; include 'includes/navbar.php' ; ?>
+   <div class="row mt-5">
+    <div class="col-12 text-center mt-5">
+      <h3>Portfolio</h3>
+    </div>
+  </div>
+  	<!--- Start Jumbotron -->
+	<div class="jumbotron">
+		<div class="container">
+			<h2 class="text-center pt-5 pb-3">BUILT WITH THE BEST</h2>
+			<div class="row justify-content-center text-center">
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/html5.png">
+						<h3>HTML5</h3>
+						<p>HTML5 is the fifth and current major version of the HTML standard, and subsumes XHTML.</p>
+					</div>
+				</div>
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/bootstrap4.png">
+						<h3>BOOTSTRAP 4</h3>
+						<p>Bootstrap is an open-source front-end library with HTML and CSS based designs.</p>
+					</div>
+				</div>
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/css3.png">
+						<h3>CSS3</h3>
+						<p>CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.</p>
+					</div>
+				</div>
+			</div><!--- End Row -->
+		</div><!--- End Container -->
+	</div>
+	<!--- End Jumbotron -->
+
+	<!--- Two Column Section -->
+	<div class="container py-3">
+		<div class="row justify-content-center py-5">
+			<div class="col-lg-6">
+				<h3 class="pb-4">Learn to build Bootstrap websites</h3>
+				<p>The columns will automatically stack on top of each other when the screen is less than 992px wide.</p>
+				<p>Resize the browser window to see the effect. Responsive web design has become more important as the amount of mobile traffic now accounts for more than half of total internet traffic.</p><a class="btn btn-purple btn-lg" href="https://w3newbie.com/courses/">W3NEWBIE COURSES</a>
+			</div>
+			<div class="col-lg-6"><img class="img-fluid" src="img/responsive.png"></div>
+		</div>
+	</div>
+	<!--- End Two Column Section -->
+  
+	<?php include 'includes/footer.php' ; ?>
+
+  <?php include 'includes/scripts.php' ; ?>
+
+</body>
+</html>
